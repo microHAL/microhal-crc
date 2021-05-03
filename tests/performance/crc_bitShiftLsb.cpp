71a0cd0bb500d772d656ffba1dc34be3294e0fc3
sakoPO <sako.po@gmail.com>
2021-05-04 00:04:42 +0200

    Added performance tests.

---
+++ tests/performance/crc_bitShiftLsb.cpp
@@ -0,0 +1,49 @@
+/**
+ * @license    BSD 3-Clause
+ * @brief      CRC library
+ *
+ * @authors    Pawel Okas
+ *
+ * @copyright Copyright (c) 2021, Pawel Okas
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ * following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <chrono>
+#include <iostream>
+
+#include "microhal-crc.hpp"
+
+#ifndef CRC_TYPE
+#error CRC_TYPE have to be defined, ie.: CRC7 or CRC16_CCITT
+#endif
+
+int main(void) {
+    std::array<uint8_t, 1'000'000> data;
+    data.fill(0xAA);
+
+    auto start = std::chrono::steady_clock::now();
+    for (size_t i = 100; i > 0; --i) {
+        [[maybe_unused]] volatile auto result = CRC_TYPE<Implementation::BitShiftLsb>::calculate(data.data(), data.size());
+    }
+    auto stop = std::chrono::steady_clock::now();
+
+    std::cout << "execution time: " << std::chrono::nanoseconds{stop - start}.count() << "ns";
+}
